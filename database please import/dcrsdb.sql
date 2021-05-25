2e5f63164bf6c8fd814182661ab84f1cb4091f41
adamz <58021562+ad4mny@users.noreply.github.com>
2021-05-25 21:59:22 +0800

    added new database

--- database please import/dcrsdb.sql
+++ database please import/dcrsdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 18, 2021 at 01:53 PM
+-- Generation Time: May 25, 2021 at 01:58 PM
 -- Server version: 8.0.23
 -- PHP Version: 8.0.3
 
@@ -45,7 +45,40 @@ CREATE TABLE `customer_data` (
 --
 
 INSERT INTO `customer_data` (`cd_id`, `cd_ud_id`, `cd_full_name`, `cd_phone`, `cd_street_1`, `cd_street_2`, `cd_postcode`, `cd_city`, `cd_state`, `cd_log`) VALUES
-(1, 1, 'adam aiman bin zulkornain', '0108884287', 'no 26 jalan perjiranan 11/18', 'bandar dato onn', 81100, 'johor bahru', 'johor', NULL);
+(2, 1, 'adam aiman bin zulkornain', '0108884285', 'no 26 jalan perjiranan 11/18', 'bandar dato onn', 81100, 'johor bahru', 'johor', '06:05:21 23/05/21'),
+(3, 7, 'siti noormaimunah binti syed hadi', '0197274670', 'no 5 lorong jalan tegak', 'kampung baru', 41020, 'wilayah persekutuan', 'kuala lumpur', NULL),
+(4, 10, 'mazliana', '0108884287', 'b4 unit 304 ', 'pangkalan tldm', 32100, 'lumut', 'perak', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_data`
+--
+
+CREATE TABLE `payment_data` (
+  `pd_id` int NOT NULL,
+  `pd_cd_id` int DEFAULT NULL,
+  `pd_rsd_id` int DEFAULT NULL,
+  `pd_txn_id` int DEFAULT NULL,
+  `pd_payment_gross` double DEFAULT NULL,
+  `pd_currency_code` varchar(10) DEFAULT NULL,
+  `pd_payer_name` varchar(50) DEFAULT NULL,
+  `pd_payer_email` varchar(50) DEFAULT NULL,
+  `pd_status` varchar(50) DEFAULT NULL,
+  `pd_log` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `payment_data`
+--
+
+INSERT INTO `payment_data` (`pd_id`, `pd_cd_id`, `pd_rsd_id`, `pd_txn_id`, `pd_payment_gross`, `pd_currency_code`, `pd_payer_name`, `pd_payer_email`, `pd_status`, `pd_log`) VALUES
+(10, 2, 13, 7, 686, 'MYR', 'Adam Zulkornain', 'adam@susundev.com', 'Completed', NULL),
+(11, 2, 14, 0, 1640, 'MYR', 'Adam Zulkornain', 'adam@susundev.com', 'Completed', NULL),
+(12, 2, 14, 0, 1640, 'MYR', 'Adam Zulkornain', 'adam@susundev.com', 'Completed', NULL),
+(13, 2, 14, 0, 1640, 'MYR', 'Adam Zulkornain', 'adam@susundev.com', 'Completed', NULL),
+(14, 2, 14, 0, 1640, 'MYR', 'Adam Zulkornain', 'adam@susundev.com', 'Completed', NULL),
+(15, 2, 14, 0, 1640, 'MYR', 'Adam Zulkornain', 'adam@susundev.com', 'Completed', NULL);
 
 -- --------------------------------------------------------
 
@@ -69,6 +102,17 @@ CREATE TABLE `repair_service_data` (
   `rsd_log` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `repair_service_data`
+--
+
+INSERT INTO `repair_service_data` (`rsd_id`, `rsd_cd_id`, `rsd_sd_id`, `rsd_status`, `rsd_progress`, `rsd_comment`, `rsd_repair_cost`, `rsd_device_brand`, `rsd_device_model`, `rsd_device_color`, `rsd_damage_severity`, `rsd_damage_info`, `rsd_log`) VALUES
+(10, 3, 1, 0, 0, NULL, NULL, 'acer', 'aspire 5', 'brown', 0, 'keyboard replacement', '06:05:02 20/05/21'),
+(11, 2, 1, 1, 2, NULL, 600, 'apple', 'macbook air 2021', 'silver', 0, 'battery replacement', NULL),
+(12, 2, NULL, 0, 0, NULL, NULL, 'acer', 'aspire 5', 'brown', 1, 'keyboard problem', NULL),
+(13, 2, 1, 1, 1, 'Repairing', 600, 'lenovo', 'legion y520', 'black', 2, 'Battery Replacement', '01:05:11 25/05/21'),
+(14, 2, 1, 1, 1, 'Repairing', 1500, 'xiaomi', 'mattebook', 'white', 2, 'Motherboard problem', '01:05:38 25/05/21');
+
 -- --------------------------------------------------------
 
 --
@@ -83,6 +127,13 @@ CREATE TABLE `runner_data` (
   `rd_plat_num` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `runner_data`
+--
+
+INSERT INTO `runner_data` (`rd_id`, `rd_ud_id`, `rd_full_name`, `rd_phone`, `rd_plat_num`) VALUES
+(1, 9, 'ahmad mamat bin malek', '0108884287', 'NDA 900');
+
 -- --------------------------------------------------------
 
 --
@@ -95,6 +146,42 @@ CREATE TABLE `staff_data` (
   `sd_full_name` varchar(80) DEFAULT NULL,
   `sd_phone` varchar(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `staff_data`
+--
+
+INSERT INTO `staff_data` (`sd_id`, `sd_ud_id`, `sd_full_name`, `sd_phone`) VALUES
+(1, 8, 'abu bin bakar ali', '0137867819');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `track_data`
+--
+
+CREATE TABLE `track_data` (
+  `td_id` int NOT NULL,
+  `td_cd_id` int DEFAULT NULL,
+  `td_rsd_id` int DEFAULT NULL,
+  `td_rd_id` int DEFAULT NULL,
+  `td_pickup_time` varchar(50) DEFAULT NULL,
+  `td_pickup_date` varchar(50) DEFAULT NULL,
+  `td_status` varchar(20) DEFAULT NULL,
+  `td_log` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `track_data`
+--
+
+INSERT INTO `track_data` (`td_id`, `td_cd_id`, `td_rsd_id`, `td_rd_id`, `td_pickup_time`, `td_pickup_date`, `td_status`, `td_log`) VALUES
+(5, 2, 13, NULL, NULL, NULL, NULL, '01:05:11 25/05/21'),
+(6, 2, 14, NULL, NULL, NULL, NULL, '01:05:22 25/05/21'),
+(7, 2, 14, NULL, NULL, NULL, NULL, '01:05:57 25/05/21'),
+(8, 2, 14, NULL, NULL, NULL, NULL, '01:05:45 25/05/21'),
+(9, 2, 14, NULL, NULL, NULL, NULL, '01:05:05 25/05/21'),
+(10, 2, 14, NULL, NULL, NULL, NULL, '01:05:38 25/05/21');
 
 -- --------------------------------------------------------
 
@@ -115,10 +202,11 @@ CREATE TABLE `user_data` (
 --
 
 INSERT INTO `user_data` (`ud_id`, `ud_usr`, `ud_pwd`, `ud_log`, `ud_created`) VALUES
-(1, 'adamny', '132c9256f087c8c267a30a22a7fa5356', NULL, NULL),
-(2, NULL, NULL, '09:05:27 18/05/21', NULL),
-(3, NULL, NULL, '09:05:38 18/05/21', NULL),
-(4, NULL, NULL, '11:05:20 18/05/21', NULL);
+(1, 'adamny', '132c9256f087c8c267a30a22a7fa5356', '11:05:34 25/05/21', '09:05:42 19/05/21'),
+(7, 'siti', 'db04eb4b07e0aaf8d1d477ae342bdff9', '07:05:11 23/05/21', '06:05:02 20/05/21'),
+(8, 'abu', '34d71b97c471931334f385b4e7b7379c', '06:05:02 20/05/21', '06:05:02 20/05/21'),
+(9, 'mamat', '24b65fcef95d94b6d41ecaa85a70e46f', NULL, NULL),
+(10, 'mazliana', 'b93cc4d98183a6d942de3e0a6ab91e4c', '10:05:52 21/05/21', '10:05:52 21/05/21');
 
 --
 -- Indexes for dumped tables
@@ -132,6 +220,14 @@ ALTER TABLE `customer_data`
   ADD KEY `cd_ud_id` (`cd_ud_id`);
 
 --
+-- Indexes for table `payment_data`
+--
+ALTER TABLE `payment_data`
+  ADD PRIMARY KEY (`pd_id`),
+  ADD KEY `payment_data_ibfk_1` (`pd_cd_id`),
+  ADD KEY `pd_rsd_id` (`pd_rsd_id`);
+
+--
 -- Indexes for table `repair_service_data`
 --
 ALTER TABLE `repair_service_data`
@@ -143,13 +239,24 @@ ALTER TABLE `repair_service_data`
 -- Indexes for table `runner_data`
 --
 ALTER TABLE `runner_data`
-  ADD PRIMARY KEY (`rd_id`);
+  ADD PRIMARY KEY (`rd_id`),
+  ADD KEY `rd_ud_id` (`rd_ud_id`);
 
 --
 -- Indexes for table `staff_data`
 --
 ALTER TABLE `staff_data`
-  ADD PRIMARY KEY (`sd_id`);
+  ADD PRIMARY KEY (`sd_id`),
+  ADD KEY `sd_ud_id` (`sd_ud_id`);
+
+--
+-- Indexes for table `track_data`
+--
+ALTER TABLE `track_data`
+  ADD PRIMARY KEY (`td_id`),
+  ADD KEY `td_cd_id` (`td_cd_id`),
+  ADD KEY `td_rd_id` (`td_rd_id`),
+  ADD KEY `td_rsd_id` (`td_rsd_id`);
 
 --
 -- Indexes for table `user_data`
@@ -165,31 +272,43 @@ ALTER TABLE `user_data`
 -- AUTO_INCREMENT for table `customer_data`
 --
 ALTER TABLE `customer_data`
-  MODIFY `cd_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `cd_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `payment_data`
+--
+ALTER TABLE `payment_data`
+  MODIFY `pd_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `repair_service_data`
 --
 ALTER TABLE `repair_service_data`
-  MODIFY `rsd_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `rsd_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `runner_data`
 --
 ALTER TABLE `runner_data`
-  MODIFY `rd_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `rd_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `staff_data`
 --
 ALTER TABLE `staff_data`
-  MODIFY `sd_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `sd_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `track_data`
+--
+ALTER TABLE `track_data`
+  MODIFY `td_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user_data`
 --
 ALTER TABLE `user_data`
-  MODIFY `ud_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ud_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Constraints for dumped tables
@@ -202,11 +321,38 @@ ALTER TABLE `customer_data`
   ADD CONSTRAINT `customer_data_ibfk_1` FOREIGN KEY (`cd_ud_id`) REFERENCES `user_data` (`ud_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `payment_data`
+--
+ALTER TABLE `payment_data`
+  ADD CONSTRAINT `payment_data_ibfk_1` FOREIGN KEY (`pd_cd_id`) REFERENCES `customer_data` (`cd_id`),
+  ADD CONSTRAINT `payment_data_ibfk_2` FOREIGN KEY (`pd_rsd_id`) REFERENCES `repair_service_data` (`rsd_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
 -- Constraints for table `repair_service_data`
 --
 ALTER TABLE `repair_service_data`
   ADD CONSTRAINT `repair_service_data_ibfk_1` FOREIGN KEY (`rsd_cd_id`) REFERENCES `customer_data` (`cd_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `repair_service_data_ibfk_2` FOREIGN KEY (`rsd_sd_id`) REFERENCES `staff_data` (`sd_id`) ON DELETE SET NULL ON UPDATE SET NULL;
+
+--
+-- Constraints for table `runner_data`
+--
+ALTER TABLE `runner_data`
+  ADD CONSTRAINT `runner_data_ibfk_1` FOREIGN KEY (`rd_ud_id`) REFERENCES `user_data` (`ud_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `staff_data`
+--
+ALTER TABLE `staff_data`
+  ADD CONSTRAINT `staff_data_ibfk_1` FOREIGN KEY (`sd_ud_id`) REFERENCES `user_data` (`ud_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `track_data`
+--
+ALTER TABLE `track_data`
+  ADD CONSTRAINT `track_data_ibfk_1` FOREIGN KEY (`td_cd_id`) REFERENCES `customer_data` (`cd_id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  ADD CONSTRAINT `track_data_ibfk_2` FOREIGN KEY (`td_rd_id`) REFERENCES `runner_data` (`rd_id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  ADD CONSTRAINT `track_data_ibfk_3` FOREIGN KEY (`td_rsd_id`) REFERENCES `repair_service_data` (`rsd_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
